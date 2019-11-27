88a47ad02cef3c908885dcbd5daad8549dd9ef34
Victor Emanuel <mrgoodye@gmail.com>
2019-11-27 13:56:37 -0300

    Updates

---
+++ precache-manifest.df67c7796681d439dc1cdd5e7dfce71e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c28c631db08508a65d3b108ca26af6",
+    "url": "/Telzir/index.html"
+  },
+  {
+    "revision": "ebec3d57b4795f1e6f15",
+    "url": "/Telzir/static/css/2.53c20c32.chunk.css"
+  },
+  {
+    "revision": "0fdec1d9738cc1c9474c",
+    "url": "/Telzir/static/css/main.c6597e5a.chunk.css"
+  },
+  {
+    "revision": "ebec3d57b4795f1e6f15",
+    "url": "/Telzir/static/js/2.cb26f359.chunk.js"
+  },
+  {
+    "revision": "0fdec1d9738cc1c9474c",
+    "url": "/Telzir/static/js/main.ebd4d845.chunk.js"
+  },
+  {
+    "revision": "345e30c08d635c66deaa",
+    "url": "/Telzir/static/js/runtime-main.4bb30182.js"
+  },
+  {
+    "revision": "6dbd1160c516570e01d696e98625d742",
+    "url": "/Telzir/static/media/011.6dbd1160.png"
+  },
+  {
+    "revision": "7b1d9eaa6386e4cc8cae8bc05f2bdd19",
+    "url": "/Telzir/static/media/016.7b1d9eaa.png"
+  },
+  {
+    "revision": "522f9c5a95f947a65510e412083315e8",
+    "url": "/Telzir/static/media/017.522f9c5a.png"
+  },
+  {
+    "revision": "456335735b826609c0c174379a49636e",
+    "url": "/Telzir/static/media/018.45633573.png"
+  },
+  {
+    "revision": "c550f7035a55bf3851f8c92d7924f87a",
+    "url": "/Telzir/static/media/120.c550f703.png"
+  },
+  {
+    "revision": "c09d80665d2498be91cb4d8c841afcff",
+    "url": "/Telzir/static/media/30.c09d8066.png"
+  },
+  {
+    "revision": "c5146a1c8528288604cf74f5894aa662",
+    "url": "/Telzir/static/media/60.c5146a1c.png"
+  },
+  {
+    "revision": "c16f671eed07bee4ed5572460a6d0890",
+    "url": "/Telzir/static/media/celular.c16f671e.png"
+  }
+]);
